1fad3b2fff029de55105dde714760f48282e5523
Cezaniltom <cezaniltom@outlook.com>
2024-08-11 18:43:16 -0300

    Projeto Finalizado

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
+
+// Animação de digitação do nome
 function typeWriter(elemento) {
     const textArray = elemento.innerHTML.split('');
     elemento.innerHTML = '';
     textArray.forEach((letra, i) => {
         setTimeout(function() {
             elemento.innerHTML += letra;
-        }, 75 * i)
+        }, 75 * i,)
     })
 }
 
 const name = document.querySelector('h2');
 typeWriter(name)
 
-// typeWriter(document.querySelector('p'));
+
+
+//Animação do slide
+let count = 1;
+document.getElementById('radio1').checked = true;
+
+setInterval( function(){
+    nextImage();
+}, 5000)
+
+function nextImage() {
+    count++;
+    if( count > 4){
+        count = 1;
+    }
+
+    document.getElementById('radio' + count).checked = true;
+
+}
