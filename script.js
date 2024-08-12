94a24e6be55a4b8b6178c64d33f5e11fd6305eb7
cezaniltom <cezaniltom@outlook.com>
2024-08-12 08:51:24 -0300

    Ajuste de margens

--- script.js
+++ script.js
@@ -21,14 +21,13 @@ document.getElementById('radio1').checked = true;
 
 setInterval( function(){
     nextImage();
-}, 5000)
+}, 7000)
 
 function nextImage() {
     count++;
     if( count > 4){
         count = 1;
     }
-
     document.getElementById('radio' + count).checked = true;
 
 }
